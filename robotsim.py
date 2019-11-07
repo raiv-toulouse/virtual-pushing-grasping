789e24c2606a72fe64dde8ebd7e7002c7839e8ca
test-moodle1 <test-moodle1.toulouse@icam.fr>
2019-11-07 13:53:24 +0100

    Ajout du programme de test qui se trouvait avant dans un fichier testmouvsim.py

--- robotsim.py
+++ robotsim.py
@@ -116,3 +116,13 @@ class RobotSim(object):
                 return gripper_fully_closed
             gripper_joint_position = new_gripper_joint_position
         gripper_fully_closed = True
+
+#
+# Programme de test
+#
+if __name__ == '__main__':
+    workspace_limits = np.asarray([[-0.724, -0.276], [-0.224, 0.224], [-0.0001, 0.4]])  # Cols: min max, Rows: x y z (define workspace limits in robot coordinates)
+    robot = RobotSim('objects/blocks', workspace_limits,True)
+    robot.move_to([-0.5, 0, 0])
+    robot.close_gripper()
+    robot.move_to([-0.5, 0, 0.5])
